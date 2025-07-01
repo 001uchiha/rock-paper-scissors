8fb0c80688e1ed3335a08e694f07e815a4fe7aa0
001uchiha <mohaking321@gmail.com>
2025-07-01 08:19:20 +0200

    Refactored game logic and upgraded to UI version

--- script.js
+++ script.js
@@ -1,69 +1,83 @@
-function getRandomChoice(arr) {
-	return arr[Math.floor(Math.random() * arr.length)].toLowerCase();
-}
+let computerScore = 0;
+let humanScore = 0;
 
-function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1);
-}
+let humanScoreSpan = document.getElementById("humanScore");
+let computerScoreSpan = document.getElementById("computerScore");
+const buttons = document.querySelectorAll(".choice");
+let resultDiv = document.getElementById("result");
+let msg = document.getElementById("result-msg");
+let finalResult = document.getElementById("finalResult");
 
 function getComputerChoice() {
-	const choices = ["Rock", "Paper", "Scissor"];
-	return getRandomChoice(choices);
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice(n) {
-	const userChoice = prompt(`Round ${n}: Enter your choice: (Rock, Paper, Scissor) `);
-	return userChoice.toLowerCase();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    msg.textContent = `You both chose ${humanChoice}`;
+    return "It's a tie!";
+  } 
+  
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    msg.textContent = `${humanChoice} beats ${computerChoice}.`;
+    return `You win!`;
+  } else {
+    computerScore++;
+    msg.textContent = ` ${computerChoice} beats ${humanChoice}.`;
+    return `You lose!`;
+  }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const roundCount = 5;
-
-
-function playGame() {
-	
-	for (let i = 1; i <= roundCount; i++) {
-		const humanSelection = getHumanChoice(i);
-		const computerSelection = getComputerChoice()
-
-		let result = playRound(humanSelection, computerSelection);
-		console.log(`You chose ${humanSelection}`);
-		console.log(`Computer chose ${computerSelection}`);
-		console.log(result);
-
-
-		if (result.startsWith('You Win')) humanScore++;
-		else if (result.startsWith('You lose')) computerScore++;
-
-		console.log(`Score: You ${humanScore} - ${computerScore} Computer\n`);
-	}
-
-
-	function playRound(humanChoice, computerChoice) {
-		if (humanChoice === computerChoice) {
-			return `It's a tie! You both chose ${capitalize(humanChoice)}.`;
-		}
-		else if (
-			(humanChoice === 'rock' && computerChoice === 'scissor') || 
-			(humanChoice === 'scissor' && computerChoice === 'paper') ||
-			(humanChoice === 'paper' && computerChoice === 'rock')
-			) {
-			 return `You Win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`;
-		}
-		else {
-			return `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`;
-		}
-	}
-
-	if (humanScore > computerScore) {
-	   console.log("🏆 You won the game!");
-	} else if (computerScore > humanScore) {
-	   console.log("💀  You lost the game.");
-	} else {
-	   console.log("🤝 The game is a tie!");
-	}
-
+function updateScore() {
+  humanScoreSpan.textContent = humanScore;
+  computerScoreSpan.textContent = computerScore;
 }
 
-playGame();
+function checkWinner() {
+  if (computerScore == 5 || humanScore == 5) {
+    showGameOverPopup();
+  
+    if (humanScore > computerScore) {
+      finalResult.textContent = "🎉 You Won!";
+      finalResult.style.color = "#0077ff";
+    } else{
+      finalResult.textContent = "💀 You lost!";
+      finalResult.style.color = "#ff4d4f";
+    }
+  }  
+}
+
+function showGameOverPopup() {
+	// Fade out game
+
+	document.getElementById("gameContainer").classList.add("fade-out");
+
+	// Show popup after short delay (so fade is visible)
+	setTimeout(() => {
+		document.getElementById("gameOverPopup").classList.remove("hidden"); 	
+	}, 500);	
+}
+
+function restartGame() { 
+	// Hide the popup
+  	document.getElementById("gameOverPopup").classList.add("hidden");
+  	// Realod the page to start over
+  	window.location.replace(window.location.href);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const humanSelection = button.dataset.choice;
+    const computerSelection = getComputerChoice();
+    result = playRound(humanSelection, computerSelection);
+    resultDiv.textContent = result;
+    updateScore();
+    checkWinner();
+  })
+})
